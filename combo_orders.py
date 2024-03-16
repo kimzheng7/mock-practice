31b8b637684fa9d9402d561eca13e0411337cb5f
kzhe6717 <kim.zheng@sydney.edu.au>
2024-03-16 23:04:07 +1100

    added guessing and timing functionality

--- combo_orders.py
+++ combo_orders.py
@@ -1,23 +1,53 @@
 import random
-print("gl with mock")
+import signal
+import sys
+import math
+successes = 0 
 
-while True:
-    stock_width = random.randint(1, 10)
-    uneven = random.randint(0, 1)
-    stock_price = random.randint(3000, 10000)
-    stock_market = [(stock_price - stock_width) / 100, (stock_price + stock_width + uneven) / 100]
-    rc = random.randint(1, 10) / 100
+def handler(a, b):
+    print("questions solved: {}".format(successes))
+    sys.exit(0)
 
-    mid_strike_index = int(stock_price // 500)
-    strikes = [5 * i for i in range(mid_strike_index - 2, mid_strike_index + 3)]
+if __name__ == "__main__":
+    signal.signal(signal.SIGALRM, handler) 
+    signal.alarm(120)
 
-    print("stock market:", stock_market[0], "@", stock_market[1])
-    print("r/c: {}".format(rc))
+    while True:
+        stock_width = random.randint(1, 10)
+        uneven = random.randint(0, 1)
+        stock_price = random.randint(3000, 10000)
+        stock_market = [(stock_price - stock_width) / 100, (stock_price + stock_width + uneven) / 100]
+        rc = random.randint(1, 10) / 100
 
-    bid_or_offer = random.choice(["bid", "offer"])
-    strike = random.choice(strikes)
-    theo = abs(stock_price / 100 - strike + rc)
-    amt_through = random.randint(-15, 15) / 100
+        mid_strike_index = int(stock_price // 500)
+        strikes = [5 * i for i in range(mid_strike_index - 2, mid_strike_index + 3)]
 
-    print("cust {} {:.2f} in the {} combos, what is this like in stock?".format(bid_or_offer, theo + amt_through, strike))
-    input()
+        print("stock market:", stock_market[0], "@", stock_market[1])
+        print("r/c: {}".format(rc))
+
+        bid_or_offer = random.choice(["bid", "offer"])
+        strike = random.choice(strikes)
+        theo = abs(stock_price / 100 - strike + rc)
+        amt_through = random.randint(-15, 15) / 100
+        
+        correct = False
+        while not correct:
+            print("cust {} {:.2f} in the {} combos, what is this like in stock?".format(bid_or_offer, theo + amt_through, strike))
+            dir_guess, price_guess = input().split()
+
+            po = strike > (stock_price / 100)
+            if po:
+                price_correct = -theo - amt_through
+            else:
+                price_correct = theo + amt_through
+            price_correct = price_correct + strike - rc
+
+            if (po and bid_or_offer == "bid") or (not po and bid_or_offer == "offer"):
+                dir_correct = "offer"
+            else:
+                dir_correct = "bid"
+
+            if math.isclose(float(price_guess), price_correct) and dir_guess == dir_correct:
+                correct = True
+
+        successes += 1
